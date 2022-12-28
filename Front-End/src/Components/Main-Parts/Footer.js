a1e3dca4a1c83e7d53174c9e34ecb079e3667eb5
Ehsan Espandar <eespandar@gmail.com>
2022-12-28 11:53:34 +0330

    F14 - footer reusable component implemented

--- Front-End/src/Components/Main-Parts/Footer.js
+++ Front-End/src/Components/Main-Parts/Footer.js
@@ -1,8 +1,55 @@
 import React from 'react'
 import styles from "./Footer.module.css";
+import logo from '../../Assets/logo-black.png'
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBBtn
+} from 'mdb-react-ui-kit';
 
 const Footer = () => {
-  return <h1>This is footer</h1>
+  return (<MDBFooter className='bg-dark bg-gradient text-white'>
+    <MDBContainer>
+      <MDBRow>
+        <MDBCol lg="6" md="12" className=' mt-mb-0'>
+          <img src={logo} alt={'logo'} className='w-25 h-100' />
+        </MDBCol>
+
+        <MDBCol lg="3" md="6" className=' mb-md-0'>
+
+          <ul className='list-unstyled mb-0'>
+            <li>
+            <MDBBtn color='secondary' className="m-1 w-50">Help</MDBBtn>
+            </li>
+            <li>
+            <MDBBtn color='secondary' className="m-1 w-50">About us</MDBBtn>
+
+            </li>
+            <li>
+            <MDBBtn color='secondary' className="m-1 w-50">FAQ</MDBBtn>
+            </li>
+          </ul>
+        </MDBCol>
+
+        <MDBCol lg="3" md="6" className=' mb-md-0'>
+
+          <ul className='list-unstyled'>
+            <li>
+            <MDBBtn color='secondary' className="m-1 w-50">Address</MDBBtn>
+
+            </li>
+            <li>
+            <MDBBtn color='secondary' className="m-1 w-50">Phone number</MDBBtn>
+
+            </li>
+          </ul>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  </MDBFooter>
+  );
 };
 
 export default Footer;
