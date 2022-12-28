b68b94ddcf15b8b686f38951e6750416292fc8fc
Ehsan Espandar <eespandar@gmail.com>
2022-12-28 12:41:53 +0330

    F14 - icons added to footer

--- Front-End/src/Components/Main-Parts/Footer.js
+++ Front-End/src/Components/Main-Parts/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import styles from "./Footer.module.css";
 import logo from '../../Assets/logo-black.png'
 import {
   MDBFooter,
   MDBContainer,
   MDBCol,
   MDBRow,
-  MDBBtn
+  MDBBtn,
+  MDBIcon
 } from 'mdb-react-ui-kit';
 
 const Footer = () => {
-  return (<MDBFooter className='bg-dark bg-gradient text-white'>
+  return (<MDBFooter className='bg-dark bg-gradient text-white position-relative'>
     <MDBContainer>
       <MDBRow>
         <MDBCol lg="6" md="12" className=' mt-mb-0'>
@@ -21,14 +21,14 @@ const Footer = () => {
 
           <ul className='list-unstyled mb-0'>
             <li>
-            <MDBBtn color='secondary' className="m-1 w-50">Help</MDBBtn>
+              <MDBBtn color='secondary' className="m-1 w-50">Help</MDBBtn>
             </li>
             <li>
-            <MDBBtn color='secondary' className="m-1 w-50">About us</MDBBtn>
+              <MDBBtn color='secondary' className="m-1 w-50">About us</MDBBtn>
 
             </li>
             <li>
-            <MDBBtn color='secondary' className="m-1 w-50">FAQ</MDBBtn>
+              <MDBBtn color='secondary' className="m-1 w-50">FAQ</MDBBtn>
             </li>
           </ul>
         </MDBCol>
@@ -37,17 +37,26 @@ const Footer = () => {
 
           <ul className='list-unstyled'>
             <li>
-            <MDBBtn color='secondary' className="m-1 w-50">Address</MDBBtn>
+              <MDBBtn color='secondary' className="m-1 w-50">Address</MDBBtn>
 
             </li>
             <li>
-            <MDBBtn color='secondary' className="m-1 w-50">Phone number</MDBBtn>
+              <MDBBtn color='secondary' className="m-1 w-50">Phone number</MDBBtn>
 
             </li>
           </ul>
         </MDBCol>
       </MDBRow>
     </MDBContainer>
+    <div className="position-absolute bottom-0 end-0">
+      <MDBBtn className='m-1' tag='a' color='dark' >
+        <MDBIcon fab icon='twitter' />
+      </MDBBtn>
+      <MDBBtn className='m-1' tag='b' color='dark' >
+        <MDBIcon fab icon='linkedin-in' />
+      </MDBBtn>
+
+    </div>
   </MDBFooter>
   );
 };
--- Front-End/src/Components/Main-Parts/Header.js
+++ Front-End/src/Components/Main-Parts/Header.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from "./Header.module.css"
 
 const Header = () => {
     return <h1>This is header</h1>
