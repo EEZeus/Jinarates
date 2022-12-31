c783f758cd7910b1a37239ea4e1cc26b72a4f7c3
mrpojam <jafari.pouya79@outlook.com>
2022-12-31 12:53:02 +0330

    implemented login system

--- Back-end/mysite/members/views.py
+++ Back-end/mysite/members/views.py
@@ -3,5 +3,17 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "the user is logged in!")
+            return redirect('login')
+        else:
+            messages.success(request, "There was an error loging in!")
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {} )
 
