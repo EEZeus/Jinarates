6e2a53a19841ca97d9e8ff5b5b6ca6bcb0e621a5
Ehsan Espandar <eespandar@gmail.com>
2023-01-02 16:26:14 +0330

    F15 - student profile page header implemented

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -4,10 +4,11 @@ import Navbar from './Components/Main-Parts/NavBar'
 import Footer from './Components/Main-Parts/Footer'
 import CoursePageHeader from './Components/Main-Parts/Headers/CoursePageHeader'
 import ProfessorPageHeader from './Components/Main-Parts/Headers/ProfessorProfilePageHeader';
+import StudentPageHeader from './Components/Main-Parts/Headers/StudentProfilePageHeader';
 function App() {
   return <React.Fragment>
     <Navbar></Navbar>
-    <ProfessorPageHeader></ProfessorPageHeader>
+    <StudentPageHeader></StudentPageHeader>
     <Footer></Footer>
   </React.Fragment>
 }
