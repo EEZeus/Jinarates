b4153d32c0fb637a6a712fe2f98d0dc05e3891ed
Ehsan Espandar <eespandar@gmail.com>
2023-01-06 22:56:06 +0330

    F16 - home page implemented

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Components/Main-Parts/NavBar'
-import Footer from './Components/Main-Parts/Footer'
-import CoursePageHeader from './Components/Main-Parts/Headers/CoursePageHeader'
-import ProfessorPageHeader from './Components/Main-Parts/Headers/ProfessorProfilePageHeader';
-import StudentPageHeader from './Components/Main-Parts/Headers/StudentProfilePageHeader';
-import UniversityPageHeader from './Components/Main-Parts/Headers/UniversityProfilePageHeader';
-import HomePage from './Components/HomePage';
+import CoursePage from './Components/CoursePage';
 function App() {
   return <React.Fragment>
-    <ProfessorPageHeader></ProfessorPageHeader>
-    <CoursePageHeader></CoursePageHeader>
-    <StudentPageHeader></StudentPageHeader>
-    <UniversityPageHeader></UniversityPageHeader>
+    <CoursePage/>
   </React.Fragment>
 }
 
