88d72a95a5734b77fe98cd7e34d64ab2f55692e3
mrpojam <jafari.pouya79@outlook.com>
2023-01-20 02:08:52 +0330

    customized user model

--- Back-end/mysite/members/models.py
+++ Back-end/mysite/members/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    
--- Back-end/mysite/mysite/settings.py
+++ Back-end/mysite/mysite/settings.py
@@ -248,4 +248,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'members.User'
+
 CSRF_COOKIE_DOMAIN = None
