ee16a8a7b3dbd6a4f7c3073beeb1367df51ff929
mrpojam <jafari.pouya79@outlook.com>
2023-01-21 12:04:29 +0330

    tested base apis

--- Back-end/mysite/members/tests.py
+++ Back-end/mysite/members/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import *
+
+class UsersTestClass(APITestCase):
+    def test_get_students(self):
+        response = self.client.get('/members/student/')
+        self.assertEqual(200, response.status_code)
+
+    def test_get_professor(self):
+        response = self.client.get('/members/professor/')
+        self.assertEqual(200, response.status_code)
+
+class UniversityTestClass(APITestCase):
+    def test_get_university(self):
+        response = self.client.get('/members/university/')
+        self.assertEqual(200, response.status_code)
+
+class CourseTestClass(APITestCase):
+       def test_get_university(self):
+        response = self.client.get('/members/course/')
+        self.assertEqual(200, response.status_code) 
